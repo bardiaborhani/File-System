ef29f1189e2fb05df0aa7f0e8003d56488640dd7
Bardia Borhani <bardia.borhani@gmail.com>
2017-11-30 22:32:07 -0800

    Create FileSystem.java

---
+++ FileSystem.java
@@ -0,0 +1,81 @@
+public class FileSystem {
+
+  private SuperBlock superblock;
+  private Directory directory;
+  pricate FileTable filetable;
+  
+  public FileSytem( int diskBlocks ) { 
+  
+    // create superblock, and format disk with inodes in default
+    superblock = new SuperBlock( diskBlocks );
+
+    // create directory, and register "/" in directory entry 0
+    directory = new Directory( superblock.inodeBlocks );
+
+    // file table is created, and store directory in the file table
+    filetable = FileTable( directory );
+  
+    // directory reconstruction
+    FileTableEntry dirEnt = open( "/" , "r" );
+    
+    int dirSize = fsize( dirEnt );
+    
+    if ( dirSize > 0 ) {
+
+      byte[] dirData = new byte[dirSize];
+      read( dirEnt , dirData );
+      directory.bytes2directory( dirData );
+      
+    }
+    
+    close( dirEnt );
+  
+  }
+ 
+ 
+  void sync() {
+  
+  }
+  
+  boolean format( int files ) {
+  
+  }
+  
+  FileTabeEntry open( String filename , String mode ){
+  
+  }
+  
+  boolean close( FileTableEntry ftEnt ){
+  
+  }
+  
+  int fsize( FileTableEntry ftEnt ){
+  
+  }
+  
+  int read( FileTableEntry ftEnt, byte[] buffer ){
+  
+  }
+  
+  int write( FileTableEntry ftEnt, byte[] buffer ){
+  
+  }
+  
+  private boolean deallocAllBlocks( FileTableEntry ftEnt ){
+  
+  }
+  
+  boolean delete( String filename ){
+  
+  }
+  
+  private final int SEEK_SET = 0;
+  private final int SEEK_CUR = 1;
+  private final int SEEK_END = 2;
+  
+  int seek( FileTableEntry ftEnt , int offset , int whence ) {
+  
+  }
+ 
+  
+}
