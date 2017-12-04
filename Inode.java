06b2502566d26514795c4f4646b027e1f203330a
Bardia Borhani <bardia.borhani@gmail.com>
2017-12-04 13:10:47 -0800

    Wrote toDisk method

--- Inode.java
+++ Inode.java
@@ -32,11 +32,50 @@ public class Inode {
       flag = SysLib.bytes2short( data , offset );
       offset += 2;
       
+      for ( int i = 0; i < directSize; i++ ){
+         direct[i] = SysLib.bytes2short( data , offset );
+         offset += 2;
+      }
+         
+      indirect = SysLib.bytes2short( data , offset );
+      offset += 2;
+      
    }
 
-   
-   int toDisk( short iNumber ) {                  // save to disk as the i-th inode
-      // design it by yourself.
+   // put the contents of this Inode object (the field variable values) into disk
+   void toDisk( short iNumber ) {                  // save to disk as the i-th inode
+           
+      // This byte array will contain all the values of this object (the field variable values)
+      // will be used later as a parameter in SysLib.rawwrite() to insert the data into disk
+      byte[] iNodeInBytes = new byte[iNodeSize];
+      
+      // Set the offset variable to index inserted information into the iNodeInBytes array
+      int offset = 0;
+      
+      SysLib.int2bytes( length , iNodeInBytes , offset );
+      offset += 4;
+      SysLib.short2bytes( count , iNodeInBytes , offset ); 
+      offset += 2;
+      SysLib.short2bytes( flag , iNodeInBytes , offset );   
+      offset += 2;
+      
+      for ( int i = 0; i < directSize; i++ ){
+         SysLib.short2bytes( direct[i] , iNodeInBytes , offset ); 
+         offset += 2;
+      }
+      
+      SysLib.short2bytes( indirect , iNodeInBytes , offset ); 
+      offset += 2;
+      
+      // Finds the block in which the inode needs to placed
+      int blockNumber = 1 + iNumber / 16;
+      
+      // Finds the location within the block that the inode needs to be placed
+      int diskOffset = ( iNumber % 16 ) * 32;
+      
+      // ...CHECK...
+      SysLib.rawwrite( blockNumber +  diskOffset , iNodeInBytes );
+      
    }
    
    short getIndexBlockNumber() {
