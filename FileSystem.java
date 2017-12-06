3408a017cc16d5e033b609f6b9273c1a23a98243
Bardia Borhani <bardia.borhani@gmail.com>
2017-12-06 00:26:55 -0800

    Update FileSystem.java

--- FileSystem.java
+++ FileSystem.java
@@ -32,46 +32,81 @@ public class FileSystem {
   
   }
  
-  // FINISH
-  void sync() {
+  // write directory information from file system to the disk
+  // it is written to the root's placement within the disk
+  public void sync() {
   
-    superblock.sync();
+    FileTableEntry rootDir = open( "/" , "w" );
     
-    //directory.directory2bytes();
+    byte[] directoryInBytes = directory.directory2bytes();
+    
+    write( rootDir , directoryInBytes );
+    
+    close( rootDir );
+    
+    superblock.sync();
     
   }
   
   // delete everything and reinstatiate 
-  boolean format( int files ) {
+  public boolean format( int files ) {
   
+    // Cannot format no number of files
+    if ( files < 0 ) {
+      return false;
+    }
+    
     superblock.format( files );
     
-    directory = new Directory(superblock.inodeBlocks);
+    directory = new Directory( superblock.inodeBlocks );
 
     // put the directory in the FileTable
-    filetable = new FileTable(directory);
+    filetable = new FileTable( directory );
     
     return true;
     
   }
   
   
+  // Opens the file specified by the fileName string in 
+  // the given mode (where "r" = ready only, "w" = write only, "w+" = read/write, "a" = append).
+  public FileTabeEntry open( String filename , String mode ){
   
-  FileTabeEntry open( String filename , String mode ){
-  
+    //The file is created if it does not exist in the mode "w", "w+" or "a".
+    // if the file is not in the directory it means it has not been created
+    if( directory.namei( filename ) == -1 ) {
+      directory.ialloc( filename );  
+    }
+   
     FileTableEntry fte = filetable.falloc( filename , mode );
+    
+    // if the file is opened to write into, deallocate all blocks
+    if (  mode == "w" && (!deallocAllBlocks( fte ))  ) {
+      return null;
+    }
+    
     return fte;
     
   }
   
   
   
-  boolean close( FileTableEntry ftEnt ){
+  // Each close() decreases the count and when the count reaches zerio, the file is no longer
+  // in use, and the file's entry is removed from the open-file table
+  public boolean synchronized close( FileTableEntry ftEnt ){
   
+    if ( ftEnt == null ) {
+      
+      return false;
+      
+    }
+    
     ftEnt.count--;
     
     if ( ftEnt.count == 0 ) { 
       
+      // once no threads are using the file - the file is no longer in use and 
+      // the fileTableEntry can be deleted
       return filetable.ffree( ftEnt );
       
     }
@@ -81,16 +116,21 @@ public class FileSystem {
   }
   
   
-  int fsize( FileTableEntry ftEnt ){
+  
+  
+  public int fsize( FileTableEntry ftEnt ){
+    
     return ftEnt.inode.length;
+    
   }
   
   
-  int read( FileTableEntry ftEnt, byte[] buffer ){
+  
+  public int read( FileTableEntry ftEnt, byte[] buffer ){
   
   }
   
-  int write( FileTableEntry ftEnt, byte[] buffer ){
+  public int write( FileTableEntry ftEnt, byte[] buffer ){
   
   }
   
@@ -98,18 +138,30 @@ public class FileSystem {
   
   }
   
-  boolean delete( String filename ){
-  
-    short iNumber = dictionary.namei(filename);
-    return ifree( iNumber );
+  // CONTINUE
+  // delete the file from the filesystem
+  // deletion is done by removing the filename from the directory and removing it from the
+  // filesystem using close()
+  public synchronized boolean delete( String filename ){
+ 
+    // grab the inode number to use to remove the file from the directory
+    short iNumber = dictionary.namei( filename );
     
+    // before deleting it needs to be checked to make sure that no threads are using the file
+    // delete the fileEntryPoint if no other threads sharing it
+    FileTableEntry ftEnt = open( filename, "r" );
+    return dictionary.ifree( iNumber );
+    
+    ..
+    ..
+    ..
   }
   
   private final int SEEK_SET = 0;
   private final int SEEK_CUR = 1;
   private final int SEEK_END = 2;
   
-  int seek( FileTableEntry ftEnt , int offset , int whence ) {
+  public int seek( FileTableEntry ftEnt , int offset , int whence ) {
   
   }
  
