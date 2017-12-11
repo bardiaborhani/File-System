7a300433d263781024b4356fab89f3b6f1693837
Bardia Borhani <bardia.borhani@gmail.com>
2017-12-10 21:35:32 -0800

    Update Directory.java

--- Directory.java
+++ Directory.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Arrays;
 
 public class Directory {
    
@@ -38,6 +38,9 @@ public class Directory {
       
    }
 
+
+
+   // CHECK
    // converts and return Directory information into a plain byte array
    // this byte array will be written back to disk
    // note: only meaningfull directory information should be converted
@@ -71,19 +74,66 @@ public class Directory {
       return dictionaryInBytes;
       
    }
-   
+
+
+
+   // converts and return Directory information into a plain byte array
+   // this byte array will be written back to disk
+   // note: only meaningfull directory information should be converted
+   // into bytes.
+   /*
+   public byte[] directory2bytes( ) {
+      
+      int offset = 0;
+      
+      int storeFsize = 4 * fsize.length;
+      int storeFnames = ( maxChars * 2 ) * fnames.length;
+      byte[] directoryInBytes = new byte[ storeFsize + storeFnames ];
+
+      for ( int i = 0; i < fsize.length; i++, offset += 4 ) {
+         
+         SysLib.int2bytes( fsize[i] , directoryInBytes , offset );   
+         
+      }
+
+      // CHECK
+      for ( int i = 0; i < fnames.length; i++, offset += maxChars * 2 ) { 
+         
+         char[] tempName = fnames[i];
+         byte[] nameInBytes = new byte[tempName.length];
+
+         for ( int j = 0; j < tempName.length; j++ ) {
+            nameInBytes[j] = (byte) tempName[j];   
+         }
+
+         //directoryInBytes[offset] = temp;      
+      }
+      
+      return directoryInBytes;
+      
+   }
+   */
+
+
+
    // filename is the one of a file to be created.
    // allocates a new inode number for this filename
    public short ialloc( String filename ) {
       
-      
+      // go through all the inode name sizes - if one is empty then there is space for an inode to allocate
       for ( short i = 0; i < fsize.length; i++ ) {
          
-         // allocate
+         // allocate if space for another inode in the directory
          if( fsize[i] == 0 ){
             
-            fsize[i] = filename.length();
-            filename.getChars( 0, fsize[i], fnames[i], 0 );
+	    // the whole length of the file name cannot be stored if it is more than the maxChars (30) length - have to shrink it - if the size is not too long then the current length can be kept
+	    if ( filename.length() > maxChars ) {
+            	fsize[i] = maxChars;
+	    } else {
+	         fsize[i] = filename.length();
+	    }
+
+            filename.getChars( 0 , fsize[i] , fnames[i] , 0 );
             return i;
             
          }
@@ -95,15 +145,19 @@ public class Directory {
       
    }
 
+  
+
+
    // deallocates this inumber (inode number)
    // the corresponding file will be deleted.
    public boolean ifree( short iNumber ) {
      
-      if ( iNumber <= 0 || iNumber > fsize.length ) {
+      if ( fsize[iNumber] <= 0 && (iNumber < 0) &&  (iNumber >= fsize.length) ) {
          
          return false;
          
       } else {
+
          // set file size to 0
          fsize[iNumber] = 0;
          return true;
@@ -113,22 +167,26 @@ public class Directory {
    }
 
    // returns the inumber corresponding to this filename
-   // ...CHECK...
    public short namei( String filename ) {
       
       // fsize.length == maxInumber
       // loop through all inodes
       for ( short i = 0; i < fsize.length; i++ ) {
-         char[] fnameString = fnames[i];
-         // converts the character array into a string
-         Arrays.toString(fnameString);
+	 // convert the name of the file from it's character array form to a string form to get ready to compare with the parameter
+	 String fnameString = new String(fnames[i] , 0 , fsize[i]);
+	// check and see if the inode's filename matches the filename indicated by the parameter
          if( fnameString.equals(filename) ){
+	    // if they match the inode associated with the filename is found and returned
             return i;   
          }
       }
       
-      // indicates that the file was not found - it does not exist in the directory
+      // indicates that the file was not found - it does not exist in the directory	
       return -1;
    }
+
+
    
 }
+
+
